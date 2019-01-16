28f2767c2a0f8a63371269eb550fb9c408d34df4
zanllp <zanllp@163.com>
2019-01-16 18:01:50 +0800

    修复已知的所有bug

--- MyTL/MyTL/main.cpp
+++ MyTL/MyTL/main.cpp
@@ -9,26 +9,42 @@ using namespace MyTL;
 using namespace std;
 int main(int argc, char ** argv)
 {
-	Stack<string> a;
+
+	//stack test
+	Stack<Stack<string>> stack;
 	if (1)
 	{
-		Stack<string> c;
-		for (size_t i = 0; i < 10000; i++)
+		for (int i = 0; i < 20; i++)
 		{
-			c.Push("hello world"+to_string(i));
+			Stack<string> stack_a;
+			for (size_t i = 0; i < 100; i++)
+			{
+				stack_a.Push("hello world" + to_string(i));
+			}
+			stack.Push(stack_a);
 		}
-		cout << c.begin().node_now.use_count() << endl;
-		a=Stack<string>(c);
-		a.print();
-		cout << c.begin().node_now.use_count() << endl;
-
+		
 	}
-	for (auto x:a)
+	for (auto a:stack)
 	{
-		cout << x << endl;
+		for (auto x : a)
+		{
+			cout << x << endl;
+		}
 	}
-
-	a.print();
-	//cout << a.GetLength()<<*a[20] << endl;
+	if (true)
+	{
+		Stack<string> s;
+		for (int i = 0; i < 10000000; i++)
+		{
+			s.Push("hello world" + to_string(i));
+		}
+		int len = s.GetLength();
+		for (int i = 0; i < len; i++)
+		{
+			s.Pop();
+		}
+		
+	}
 	system("pause");
 }
